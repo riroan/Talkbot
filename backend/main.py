856a4cd94893bbc7551409a4effc56677519430b
riroan <riroan@naver.com>
2023-08-25 21:03:21 +0900

    cors

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from util import get_next_answer
 from pydantic import BaseModel
 
 class Message(BaseModel):
     msg: str
-
+origins = [
+    "*"
+]
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/")
 async def get_answer(message: Message):
